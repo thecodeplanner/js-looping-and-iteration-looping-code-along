69b14b3dc847ce7d259c3028064eb3bd36a107c0
thecodeplanner <thecodeplanner@gmail.com>
2020-12-09 17:37:08 -0500

    commit tree fdcb8ef1147bc1c83ba6036519f2b8c4d9880b84

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+
+
+function writeCards(arrayOfNames, eventNames) {
+    let cards = []
+    for (let i = 0; i < arrayOfNames.length; i++) {
+        cards.push(`Thank you, ${arrayOfNames[i]}, for the wonderful ${eventNames} gift!`);
+    }
+    return cards;
+}
+
+function countDown(number) {
+    let count = number;
+    while (count >= 0) {
+        console.log(count--);
+    }
+
+}
